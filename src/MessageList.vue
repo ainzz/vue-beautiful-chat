a329a38136df8f13189273d7d52701e121923f21
ainz <alintanase92@gmail.com>
2019-05-07 13:18:57 +0300

    fixed default message

--- src/MessageList.vue
+++ src/MessageList.vue
@@ -1,87 +1,102 @@
 <template>
-  <div class="sc-message-list" ref="scrollList" :style="{backgroundColor: colors.messageList.bg}">
-    <Message v-for="(message, idx) in messages" :message="message" :chatImageUrl="chatImageUrl(message.author)" :authorName="authorName(message.author)" :key="idx" :colors="colors" :messageStyling="messageStyling" />
-    <Message v-show="showTypingIndicator !== ''" :message="{author: showTypingIndicator, type: 'typing'}" :chatImageUrl="chatImageUrl(showTypingIndicator)" :colors="colors" :messageStyling="messageStyling" />
-	<Message v-if="messages.length < 1" message="Nu exista conversatii. Poti incepe una chiar acum!" type="system" authorName="me" />
-  </div>
+	<div class="sc-message-list" ref="scrollList" :style="{backgroundColor: colors.messageList.bg}">
+		<Message v-for="(message, idx) in messages" :message="message" :chatImageUrl="chatImageUrl(message.author)"
+			:authorName="authorName(message.author)" :key="idx" :colors="colors" :messageStyling="messageStyling" />
+		<Message v-show="showTypingIndicator !== ''" :message="{author: showTypingIndicator, type: 'typing'}"
+			:chatImageUrl="chatImageUrl(showTypingIndicator)" :colors="colors" :messageStyling="messageStyling" />
+		<Message v-if="messages.length < 1" :message="defaultMessage" authorName="me" :colors="colors" :messageStyling="messageStyling" />
+	</div>
 </template>
 <script>
-import Message from './Message.vue'
-import chatIcon from './assets/chat-icon.svg'
+	import Message from './Message.vue'
+	import chatIcon from './assets/chat-icon.svg'
 
-export default {
-  components: {
-    Message
-  },
-  props: {
-    participants: {
-      type: Array,
-      required: true
-    },
-    messages: {
-      type: Array,
-      required: true
-    },
-    showTypingIndicator: {
-      type: String,
-      required: true
-    },
-    colors: {
-      type: Object,
-      required: true
-    },
-    alwaysScrollToBottom: {
-      type: Boolean,
-      required: true
-    },
-    messageStyling: {
-      type: Boolean,
-      required: true
-    }
-  },
-  methods: {
-    _scrollDown () {
-      this.$refs.scrollList.scrollTop = this.$refs.scrollList.scrollHeight
-    },
-    shouldScrollToBottom() {
-      return this.alwaysScrollToBottom || (this.$refs.scrollList.scrollTop > this.$refs.scrollList.scrollHeight - 600)
-    },
-    profile(author) {
-      const profile = this.participants.find(profile => profile.id === author)
+	export default {
+		components: {
+			Message
+		},
+		props: {
+			participants: {
+				type: Array,
+				required: true
+			},
+			messages: {
+				type: Array,
+				required: true
+			},
+			showTypingIndicator: {
+				type: String,
+				required: true
+			},
+			colors: {
+				type: Object,
+				required: true
+			},
+			alwaysScrollToBottom: {
+				type: Boolean,
+				required: true
+			},
+			messageStyling: {
+				type: Boolean,
+				required: true
+			},
+			noMessagesText: {
+				type: String,
+				default: 'Nu exista conversatii. Poti incepe una chiar acum!'
+			}
+		},
+		methods: {
+			_scrollDown() {
+				this.$refs.scrollList.scrollTop = this.$refs.scrollList.scrollHeight
+			},
+			shouldScrollToBottom() {
+				return this.alwaysScrollToBottom || (this.$refs.scrollList.scrollTop > this.$refs.scrollList.scrollHeight -
+					600)
+			},
+			profile(author) {
+				const profile = this.participants.find(profile => profile.id === author)
 
-      // A profile may not be found for system messages or messages by 'me'
-      return profile || {imageUrl: '', name: ''}
-    },
-    chatImageUrl(author) {
-      return this.profile(author).imageUrl
-    },
-    authorName(author) {
-      return this.profile(author).name
-    }
-  },
-  computed: {
-    defaultChatIcon() {
-      return chatIcon
-	},
-	orderedMessages() {
-		
+				// A profile may not be found for system messages or messages by 'me'
+				return profile || {
+					imageUrl: '',
+					name: ''
+				}
+			},
+			chatImageUrl(author) {
+				return this.profile(author).imageUrl
+			},
+			authorName(author) {
+				return this.profile(author).name
+			}
+		},
+		computed: {
+			defaultChatIcon() {
+				return chatIcon
+			},
+			defaultMessage() {
+				return {
+					type: 'system',
+					data: {
+						text: this.noMessagesText,
+					}
+				}
+			}
+		},
+		mounted() {
+			this._scrollDown()
+		},
+		updated() {
+			if (this.shouldScrollToBottom())
+				this.$nextTick(this._scrollDown())
+		}
 	}
-  },
-  mounted () {
-    this._scrollDown()
-  },
-  updated () {
-    if (this.shouldScrollToBottom())
-      this.$nextTick(this._scrollDown())
-  }
-}
 </script>
 
 <style scoped>
-.sc-message-list {
-  height: 80%;
-  overflow-y: auto;
-  background-size: 100%;
-  padding: 40px 0px;
-}
+	.sc-message-list {
+		height: 80%;
+		overflow-y: auto;
+		background-size: 100%;
+		padding: 40px 0px;
+	}
 </style>
